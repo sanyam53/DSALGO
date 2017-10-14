c66d7265c0073ac78055849accd837fdd94c1a11
tolani <tolani.mahesh72@gmail.com>
2017-10-14 23:48:10 +0530

    deletion implementd in AVL tree, look at notes u ve written on insight behind insertion & deletion in avl tree.

--- src/com/tolani/AVLtree/AvlTree.java
+++ src/com/tolani/AVLtree/AvlTree.java
@@ -138,6 +138,127 @@ public class AvlTree {
     }
 
 
+    public Node delete(Node root,int key)     // u dnt wnt to return a ptr in case of deletin but it ll be useful for recursin within d functin
+    {
+        // if u use search functin here and drctly get to tht node then problem with the deletion
+        // problem is as u dnt hv parent ptr in ur impn , how u reach to d parent , so impn delete fun carefully
+
+        Node treeRoot = root;
+        if(root == null) return null;
+
+        else if(root.data < key)
+        {
+            root.right = delete(root.right,key);
+        }
+        else if(root.data > key)
+        {
+            root.left = delete(root.left,key);
+        }
+
+        else           // this means u r reached at the node u wnt to delete means root.data == key , now chck fr the 3 cases of deletion
+        {
+            if(root.left == null)      // if left ptr is null  OR  if bth will be null then it is returning a right ptr means returning null
+            {
+                // in this case u delete the node and just join the right child if right child is not null
+                return root.right;
+            }
+
+            else if(root.right == null)   // if right ptr is null
+            {
+                return root.left;
+            }
+
+            else                      // both r not null
+            {
+                // so u find the inorder predecesor of a node , u swap the value of it wid the node we wnt to delete and delete the predecsr node
+
+                Node prede = inorderPredecessor(treeRoot,root.data);
+                swap(prede, root);
+
+                // deleting inorder predecessor now : we knw tht it is on the left side so we drctly call root.left for efficiency
+
+                root.left = delete(root.left, prede.data);
+            }
+        }
+
+
+        root.height = updateHeight(root);
+
+        int balance = checkBalance(root);
+
+       // balance is > 1 means lst height is more than rst height , means we deletd smthng from rst , bcz we r chckng balance aftr deletion
+        // now so two cases in this case : LL case (rotate right) and LR case ( rotate left then right )
+        // now to chck tht we will check the left subtree : if it has balance factor >=0 means (0 or 1) means it is LL CASE
+        // If it is < 0 (means -1 : here it cn be -1 only bcz we searched for first imbalanced node cmng frm the bottom so
+        // balance factor of root.left cnt be -2 , so if it is -1 means rst has more and lst has less now if we drctly rotate right on root
+        // then imbalance will happen : try to do it , so first rotate left on root.left and then rotate right on root
+
+        if(balance > 1 && checkBalance(root.left) >=0) {
+            return rightRotate(root);
+        }
+
+        // Left Right Case
+        if (balance > 1 && checkBalance(root.left) < 0)
+        {
+            root.left = leftRotate(root.left);
+            return rightRotate(root);
+        }
+
+        // Right Right Case
+        if (balance < -1 && checkBalance(root.right) <= 0)
+            return leftRotate(root);
+
+        // Right Left Case
+        if (balance < -1 && checkBalance(root.right) > 0)
+        {
+            root.right = rightRotate(root.right);
+            return leftRotate(root);
+        }
+
+        return root;
+    }
+
+    public Node inorderPredecessor(Node root,int k)
+    {
+        Node t;
+        Node key = search(root,k);    // we are returning a ptr to tht node in a tree so we cn traverse ahead
+
+        if(key.left != null)
+        {
+            t = key.left;
+
+            while(t.right != null)
+            {
+                t = t.right;
+            }
+
+            return t;
+        }
+
+        return null;
+    }
+
+     public Node search(Node root,int key)
+    {
+        if(root == null) return null;
+
+        if(root.data == key) return root;
+
+        else if (root.data < key) return search(root.right,key);
+
+        else     // tc : if u put everythng as an elif then u ve to explicitly gv return statemnt. !
+        {
+            return search(root.left,key);
+        }
+
+    }
+
+    private void swap(Node a , Node b) {
+        int tempdata = a.data;
+        a.data = b.data;
+        b.data = tempdata;
+    }
+
     public void levelOrderTraversal(Node root)
     {
         Queue<Node> q = new LinkedList<Node>();   // refer geeksforgeeks for understndng Queue in java
--- src/com/tolani/AVLtree/Test_avltree.java
+++ src/com/tolani/AVLtree/Test_avltree.java
@@ -6,6 +6,8 @@ public class Test_avltree {
     {
         AvlTree at = new AvlTree();
 
+        // take care here tht root also can be changed while rotations so dnt create one root and work on it as u did in bst ! keep ur eyes and mind open
+
         at.root = at.insert(at.root,10);
         at.root = at.insert(at.root,20);
         at.root = at.insert(at.root,30);
@@ -13,6 +15,16 @@ public class Test_avltree {
         at.root = at.insert(at.root,50);
         at.root = at.insert(at.root,25);
 
+        at.levelOrderTraversal(at.root);
+
+        at.root = at.delete(at.root,50);
+        System.out.println();
+        at.levelOrderTraversal(at.root);
+        at.root = at.delete(at.root,40);
+        System.out.println();
+        at.levelOrderTraversal(at.root);
+        at.root = at.delete(at.root,10);
+        System.out.println();
         at.levelOrderTraversal(at.root);
 
 
