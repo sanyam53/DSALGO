56590144a5b87847928c69fddb5f36f5831d2872
tolani <tolani.mahesh72@gmail.com>
2017-11-16 15:20:56 +0530

    Dijkstra single source shortest path impn > some changes are : priority field added in  vertex class > AdjList of each vertex has been modified to be AdjListNode which contains vertex id + weight of an edge

--- src/com/tolani/Graphs/GenericAlgo.java
+++ src/com/tolani/Graphs/GenericAlgo.java
@@ -8,9 +8,10 @@ import static com.tolani.Graphs.Color.*;
 
 public class GenericAlgo {
 
-    public static void main(String[] args)
-    {
-        Graph2_v2 g = new Graph2_v2(5,GraphType.DIRECTED);       // size 5 means vertices frm 0 to 4 : tc here
+    public static void main(String[] args) {
+        Graph2_v2 g = new Graph2_v2(5, GraphType.DIRECTED);       // size 5 means vertices frm 0 to 4 : tc here
+/*
+        non weighted graph example : use for dfs ,bfs
 
         g.addEdge(0,1);
         g.addEdge(0,3);
@@ -22,11 +23,39 @@ public class GenericAlgo {
 
         g.displayGraph();
 
+
         //BFS(g,0);
         System.out.println();      // if u call BFS first and then DFS then colors will change so take care dnt call both together here
-        DFS(g,0);
+        //DFS(g,0);
+
+        //DFSrec(g,0);
+
+*/
+
+        // weighted graph example
+
+        g.addWeightedEdge(0, 1, 3);
+        g.addWeightedEdge(0, 3, 6);
+        g.addWeightedEdge(1, 3, 2);
+        g.addWeightedEdge(1, 2, 9);
+        g.addWeightedEdge(3, 4, 4);
+        g.addWeightedEdge(4, 1, 2);
+        g.addWeightedEdge(4, 2, 1);
+
+        g.displayGraph();
+
+        DijkstraSSSP(g,0);
+
+        // printing result of dijsktra : edges thr in the graph will be (pi(v) , v) , and priority(v) is weight of shortes path frm src to 'v'
+        for(Vertex v : g.vertexList.values())
+        {
+            System.out.print(v.piValue + " " + v.data + " " + v.getPriority());
+            System.out.println();
+        }
+
     }
 
+    // Graph Traversals : bfs , dfs , dfs recursive
     public static void BFS(Graph2_v2 g ,int source)
     {
         // Java Note : Queue is an interface so u cant instantiate a queue , so it is usually implemented as a priority queue or a linkedlist
@@ -73,9 +102,9 @@ public class GenericAlgo {
 
             System.out.print(u.data + " ");
 
-            for(Integer x : u.adjList)
+            for(AdjListNode x : u.adjList)
             {
-                Vertex vObj = g.vertexList.get(x);
+                Vertex vObj = g.vertexList.get(x.getVertexId());
 
                 if(vObj.getColor() == GREEN)
                 {
@@ -127,13 +156,16 @@ public class GenericAlgo {
         while(!s.empty())       // s.empty() returns true if stack contains no items
         {
             Vertex u = (Vertex) s.pop();      // delete a vertex frm stack AND set its color as red one
+
+            if(u.getColor() == BLACK) {
+                System.out.print(u.data + " ");        // u r printing this bcz u r gonna explore this vertex next : means explrng neighbrs of it
+            }
+
             u.setColor(RED);
 
-            System.out.print(u.data + " ");
-
-            for(Integer x : u.adjList)
+            for(AdjListNode x : u.adjList)
             {
-                Vertex vObj = g.vertexList.get(x);
+                Vertex vObj = g.vertexList.get(x.getVertexId());
 
                 if(vObj.getColor() == GREEN)
                 {
@@ -144,10 +176,102 @@ public class GenericAlgo {
 
                 else if(vObj.getColor() == BLACK)
                 {
-                    // WE WILL DO SOMETHING HERE
+                    // so we didnt do anything in case of queue here bcz if elemnt is already thr in ds , and in case of queue if u add it again
+                    // then it wont help you , but in case of stack priority will change on adding again , so we are adding an element again
+
+                    s.push(vObj);
+                    vObj.setColor(BLACK);
+                    vObj.piValue = u.data;
                 }
             }
 
         }
     }
+
+    public static void DFSrec(Graph2_v2 g , int source)
+    {
+        Vertex src = g.vertexList.get(source);
+
+        DFSrecursive(g,src);
+    }
+
+    public static void DFSrecursive(Graph2_v2 g , Vertex u)        // v is the vertex which U WNT to add onto the stack right now
+    {
+        u.setColor(BLACK);                // u mark this vertex as added to the DS and print it
+        System.out.print(u.data + " ");    // u r printing it as u r popping it and visiting its neighbours : meanss u r exploring this vertex
+
+        for(AdjListNode x : u.adjList)         // so u will call func recursively for all the adjacent vertices if they are nt added to d DS yet
+        {
+            Vertex vObj = g.vertexList.get(x.getVertexId()) ;
+
+          if(vObj.getColor() == GREEN)
+            {
+                vObj.piValue = u.data;
+                DFSrecursive(g,vObj);
+            }
+        }
+
+        u.setColor(RED);   // u ve finished visiting all of the neighbours so u r out of the loop
+    }
+
+    // Dijkstra's shortest path algorithm
+
+    public static void DijkstraSSSP(Graph2_v2 g , int source)
+    {
+       Vertex src = g.vertexList.get(source);
+
+//You should create your priority Queue a little bit different by specifying how its elements should be compared. That is done by passing an anonymous Comparator for the Vertex class:
+
+       PriorityQueue<Vertex> minHeap = new PriorityQueue<Vertex>(g.noOfVertices, new Comparator<Vertex>() {
+           @Override
+           public int compare(Vertex v1, Vertex v2) {
+               return Integer.compare(v1.getPriority(),v2.getPriority());
+           }
+       });
+
+       src.setPriority(0);
+       minHeap.add(src);
+       visitDijkstra(g,minHeap,src);
+
+    }
+
+    public static void visitDijkstra(Graph2_v2 g,PriorityQueue minHeap,Vertex v)
+    {
+      while(!minHeap.isEmpty())
+      {
+
+          Vertex u =(Vertex) minHeap.remove();
+
+          u.setColor(RED);
+
+          for(AdjListNode adjNode : u.adjList)
+          {
+                Vertex vObj = g.vertexList.get(adjNode.vertexId);
+
+                if(vObj.getColor() == GREEN)
+                {
+                    vObj.setPriority( u.getPriority() + adjNode.getWeight());        // P(v) = P(u) + W(u,v);
+                    minHeap.add(vObj);
+                    vObj.setColor(BLACK);
+                    vObj.piValue = u.data;
+                }
+
+                else if(vObj.getColor() == BLACK)        // if node is already thr in the data structure
+                {
+                    if(vObj.getPriority() > ( u.getPriority() + adjNode.getWeight()))    // then u calculate the priority if > then this then update
+                    {
+                        vObj.setPriority(u.getPriority() + adjNode.getWeight());       // this is actually a decrease key function
+                        // so now to change into the data structure we dnt hv in built function in java so we remove then change priority then reinsert it into the heap
+
+                        minHeap.remove(vObj);
+                        minHeap.add(vObj);
+
+                        vObj.piValue = u.data;
+                    }
+                }
+          }
+      }
+
+    }
+
 }
--- src/com/tolani/Graphs/Graph2_v2.java
+++ src/com/tolani/Graphs/Graph2_v2.java
@@ -4,7 +4,6 @@
 *   the structure of a node contains integer data and a color < this color is useful while implementing BFS, DFS, Dijkstra, Min. spanning tree
 * */
 
-
 package com.tolani.Graphs;
 import java.util.*;
 
@@ -17,9 +16,12 @@ public class Graph2_v2 {
     public static class Vertex
     {
         int data;
-        ArrayList<Integer> adjList;       // this will contain integer of the vertices that are connected to this vertex object
+        ArrayList<AdjListNode> adjList;       // this will contain integer of the vertices that are connected to this vertex object
         Color color;      // Color is an enum type
         int piValue;
+
+        int priority;
+
 
         Vertex(int data)
         {
@@ -27,7 +29,9 @@ public class Graph2_v2 {
             this.color = Color.GREEN;       // Initalization with green color
             this.piValue = -1;
 
-            adjList = new ArrayList<>();     // intializing
+            adjList = new ArrayList<AdjListNode>();     // intializing
+
+            this.priority = Integer.MAX_VALUE;
         }
 
         public void setColor(Color c)
@@ -39,6 +43,55 @@ public class Graph2_v2 {
         {
             return this.color;
         }
+
+        public int getPriority() {
+            return priority;
+        }
+
+        public void setPriority(int priority) {
+            this.priority = priority;
+        }
+    }
+
+
+    /*  the thing is in case of the weighted graph when we explore the adjacent vertices , we also want the weight of the correspndng edge
+     *   now , thr r some options we can try are : 1. 2d array which contains weight of edges bw (u,v) : this is O(v^2 space) not a good idea
+     *                                             2. we can create a class EDGE , and we can create maintain list of all edges : O(2*E)
+     *                                             3. we can maintain adjacency list of each vertex which contains Edge Objects : O(2*E)
+     *                                             4. for now we can modify the adjacency list node tht is in the adjList of each vertex
+     *                                                 , we will create one class adjListNode , and we wr befr storing only an integer reltd
+     *                                                 to the vertex we wnt to fetch , but now we will add one more parameter weight into it
+     *                                                 this will save some space and this wont tk any search time for an edge weight (u,v)
+     *                                                 we will hv it in O(1) : simultaneously
+    * */
+
+    public static class AdjListNode
+    {
+        int vertexId;
+        int weight;
+
+        AdjListNode(int vid)
+        {
+            vertexId = vid;
+            weight = -1;         // if it is a non weighted edges then u use this constructor and u define weight as -1 here
+        }
+
+        AdjListNode(int vid , int w)
+        {
+            this.vertexId = vid;
+            this.weight = w;
+        }
+
+        public int getVertexId()
+        {
+            return this.vertexId;
+        }
+
+        public int getWeight()
+        {
+            return this.weight;
+        }
+
     }
 
     Graph2_v2(int size,GraphType type)
@@ -55,21 +108,50 @@ public class Graph2_v2 {
         }
     }
 
+    // below addEdge method is for non weighted graph
+
     public void addEdge(int u , int v)       // we ve passed a graph obj in case u working with multiple graph objects
     {
         if(this.gtype == GraphType.DIRECTED)
         {
             Vertex uObj =  vertexList.get(u);    // this will return a corresponding vertex object
-            uObj.adjList.add(v);                // now u access the adjacency list of tht vertex obj and add the correspndng edge in it
+            AdjListNode adjNode = new AdjListNode(v);
+            uObj.adjList.add(adjNode);                // now u access the adjacency list of tht vertex obj and add the correspndng edge in it
         }
 
         else     // if graph is undirected u add edges twice
         {
            Vertex uObj = vertexList.get(u);
-           uObj.adjList.add(v);
+            AdjListNode adjNode = new AdjListNode(v);
+           uObj.adjList.add(adjNode);
 
            Vertex vObj = vertexList.get(v);
-           vObj.adjList.add(u);
+           adjNode = new AdjListNode(u);
+           vObj.adjList.add(adjNode);
+        }
+
+    }
+
+    public void addWeightedEdge(int u, int v, int weight)       // we ve passed a graph obj in case u working with multiple graph objects
+    {
+        if(this.gtype == GraphType.DIRECTED)
+        {
+            Vertex uObj =  vertexList.get(u);    // this will return a corresponding vertex object
+
+            AdjListNode adjNode = new AdjListNode(v,weight);       // 'v' is an int represntng vertex 'v' , and weight is (u,v) edge weight
+
+            uObj.adjList.add(adjNode);           // now u access the adjacency list of tht vertex obj and add the correspndng edge in it
+        }
+
+        else     // if graph is undirected u add edges twice
+        {
+           Vertex uObj = vertexList.get(u);
+           AdjListNode adjNode = new AdjListNode(v,weight);
+           uObj.adjList.add(adjNode);
+
+           Vertex vObj = vertexList.get(v);
+           adjNode = new AdjListNode(u,weight);     // u cn use same reference
+           vObj.adjList.add(adjNode);
         }
 
     }
@@ -79,24 +161,29 @@ public class Graph2_v2 {
 
     public void displayGraph()
     {
-       for(Vertex v : vertexList.values())
+       for(Vertex v : vertexList.values())         // retrieving all the vertices one by one
        {
            System.out.print(v.data + " => ");
 
-            Iterator i = v.adjList.iterator();
+            Iterator i = v.adjList.iterator();          // retrieving adjacency list of the one vertex
 
             while(i.hasNext())
             {
-                System.out.print(i.next() + " -> ");
+                AdjListNode n = (AdjListNode) i.next();      // printing the adjList Object
+
+                if(n.weight == -1)
+                {
+                    System.out.print(n.getVertexId() + " -> ");
+                }
+                else
+                {
+                    System.out.print("v:" + n.getVertexId() + "|w:" + n.getWeight() + "  ->  ");
+                }
             }
 
             System.out.println();
        }
     }
-
-
-
-
 }
 
 enum Color
