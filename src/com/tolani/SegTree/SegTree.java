9ca4caec567dba6798946b92c8fa1bce6de9979e
tolani <tolani.mahesh72@gmail.com>
2017-11-21 22:40:48 +0530

    segment tree implemented !

--- src/com/tolani/SegTree/SegTree.java
+++ src/com/tolani/SegTree/SegTree.java
@@ -9,6 +9,8 @@ public class SegTree {
     int segTreeRangeLow;
     int segTreeRangeHigh;
 
+    int[] lazySegArr;
+
     /*
        // lets first decide the size of the seg tree : so if ip arr size is in pow of 2 then u create a seg tree of size (2*size -1)
         // if size isnt in pow of 2 then u create seg tree of size = 2*( nxt value tht is pow of 2 , aftr the size of ip arr tht is gvn) -1
@@ -25,7 +27,7 @@ public class SegTree {
 
 // constructor in which we allocate the size and call buildSegTree method
 
-    SegTree(int[] ipArr, Operation operation)       // u will pass the ip arr & and object of an Operation by which segTree is defined
+    SegTree(int[] ipArr, Operation operation , boolean isLazy)       // u will pass the ip arr & and object of an Operation by which segTree is defined
     {
         // height of a full binary tree in terms of no of leaves 'N' : which are elements of an ip arr
 
@@ -42,7 +44,14 @@ public class SegTree {
         // storing the interval tht segemnt tree representing of an ip array
         segTreeRangeLow =0;
         segTreeRangeHigh = (ipArr.length - 1);
+
+        // if it is lazy propagation impn then create a lazy seg tree of same size : intialized with all 0s
+        if(isLazy == true)
+        {
+            lazySegArr = new int[segTreeSize];
+        }
     }
+
 
     // method for building a segment tree
 
@@ -114,7 +123,167 @@ public class SegTree {
         }
     }
 
+    public void updateQuery(int[] ipArr, int index , int value, Operation op)
+    {
+        ipArr[index] = value;
+        updateQuery2(segTreeRangeLow,segTreeRangeHigh,index,value,0,op);
+    }
 
+    public void updateQuery2(int segRangeLow,int segRangeHigh,int index,int value,int pos,Operation operation)
+    {
+        // if index tht need to be updated is < the lowRange of a node or > the highRange of a node then u return , and wont go furthr in tree
+
+        if(index < segRangeLow || index > segRangeHigh)
+        {
+            return;
+        }
+
+        // when low == high in the segment tree and it combined with abv if conditin : it will also be equal to the 'index' : so update the value
+
+        if(segRangeHigh == segRangeLow)
+        {
+            segArr[pos] = value;
+        }
+
+        else
+        {
+            int mid = (segRangeHigh + segRangeLow) /2;
+
+            updateQuery2(segRangeLow,mid,index,value,2*pos + 1,operation);
+
+            updateQuery2(mid+1,segRangeHigh,index,value,2*pos + 2,operation);
+
+            segArr[pos] = operation.perform(segArr[2*pos + 1] ,segArr[2*pos +2]);
+
+        }
+
+
+    }
+
+
+    // lazy propagation : 2 operations mainly : 1. range Updates  , 2. range query  ( range query cn be a single node query is range is like 0-0
+
+    public void rangeUpdateLazy(int[] ipArr , int qlow , int qhigh ,int delta , Operation op)        // u wnt to update by delta in given range
+    {
+        rangeUpdateLazy2(qlow,qhigh,segTreeRangeLow, segTreeRangeHigh ,0 , delta, op);
+    }
+
+    public void rangeUpdateLazy2(int qlow , int qhigh , int segRangeLow , int segRangeHigh ,int pos , int delta,Operation op ) {
+
+        if(segRangeLow > segRangeHigh)
+        {
+            return;
+        }
+
+        // additional code for lazy propagation : u chck the pos u are at in lazySegTree , and if it is not 0 means , some update is pending at
+        // this pos u r at, so complete tht update first then go further
+
+        if(lazySegArr[pos] !=0)
+        {
+            segArr[pos] += lazySegArr[pos];
+
+            if(segRangeLow != segRangeHigh)     // making children lazy if present
+            {
+                lazySegArr[2*pos+1] += lazySegArr[pos];   // pass on the value stored in the parent to the children
+                lazySegArr[2*pos+2] += lazySegArr[pos];
+            }
+
+            lazySegArr[pos] = 0;   // making the parent node unlazy as it has done his work ! good boy
+        }
+
+
+
+        // usual updateQuery code for segTree starts from below
+
+        // no overlap
+        if (qlow > segRangeHigh || qhigh < segRangeLow) {
+            return;
+        }
+
+        // total overlap
+        if (qlow <= segRangeLow && qhigh >= segRangeHigh)
+        {
+            // update that node by delta and make children lazy (if present) : if it is a leaf node then update and leave
+
+            segArr[pos] += delta;
+
+            if(segRangeLow != segRangeHigh)         // to check tht it is not a leaf node and hv 2 children
+            {
+                lazySegArr[2 * pos + 1] += delta;
+                lazySegArr[2 * pos + 2] += delta;
+            }
+        }
+
+        else
+        {
+            int mid = ( segRangeLow + segRangeHigh )/2;
+
+            rangeUpdateLazy2(qlow,qhigh,segRangeLow,mid,2*pos+1,delta,op);
+            rangeUpdateLazy2(qlow,qhigh,mid+1,segRangeHigh,2*pos+2,delta,op);
+
+            segArr[pos] = op.perform(segArr[2*pos+1],segArr[2*pos+2]);
+        }
+
+    }
+
+    public int rangeQueryLazy(int[] ipArr, int qlow , int qhigh , Operation op)
+    {
+        return rangeQueryLazy2(qlow,qhigh,segTreeRangeLow,segTreeRangeHigh,0,op);
+    }
+
+    public int rangeQueryLazy2(int qlow , int qhigh , int segRangeLow, int segRangeHigh , int pos , Operation op)
+    {
+        if(segRangeLow > segRangeHigh)
+        {
+            return op.returnValue();
+        }
+
+        // additional code for lazy propagation : u chck the pos u are at in lazySegTree , and if it is not 0 means , some update is pending at
+        // this pos u r at, so complete tht update first then go further
+
+        if(lazySegArr[pos] !=0)
+        {
+            segArr[pos] += lazySegArr[pos];
+
+            if(segRangeLow != segRangeHigh)     // making children lazy if present
+            {
+                lazySegArr[2*pos+1] += lazySegArr[pos];   // pass on the value stored in the parent to the children
+                lazySegArr[2*pos+2] += lazySegArr[pos];
+            }
+
+            lazySegArr[pos] = 0;   // making the parent node unlazy as it has done his work ! good boy
+        }
+
+
+
+        // usual Query code for segTree starts from below
+
+        // no overlap
+        if (qlow > segRangeHigh || qhigh < segRangeLow) {
+            return op.returnValue();
+        }
+
+        // total overlap : here code wont change as we changed in the updateLazy function bcz while querying we dnt hv to go down simple
+        if (qlow <= segRangeLow && qhigh >= segRangeHigh)
+        {
+            return segArr[pos];
+        }
+
+        else
+        {
+            int mid = ( segRangeLow + segRangeHigh )/2;
+
+            int val1 = rangeQueryLazy2(qlow,qhigh,segRangeLow,mid,2*pos+1,op);
+            int val2 = rangeQueryLazy2(qlow,qhigh,mid+1,segRangeHigh,2*pos+2,op);
+
+            return op.perform(val1,val2);
+        }
+    }
+
+    public void displayLazyTree()
+    {
+        System.out.println(Arrays.toString(lazySegArr));
+    }
 }
 
 
--- src/com/tolani/SegTree/Test_SegTree.java
+++ src/com/tolani/SegTree/Test_SegTree.java
@@ -1,5 +1,7 @@
 package com.tolani.SegTree;
 
+import java.util.Arrays;
+
 public class Test_SegTree {
 
     public static void main(String[] args)
@@ -8,11 +10,32 @@ public class Test_SegTree {
         Operation sumop = new SumOperation();
 
         // so u pass an ipArr and the operation on which u wnt to define a seg tree
-
-        SegTree st = new SegTree(ipArr,sumop);
+/*
+        SegTree st = new SegTree(ipArr,sumop,false);
 
         st.displaySegTree();
 
         System.out.println(st.rangeQuery(ipArr,0,2,sumop));
+
+        st.updateQuery(ipArr,0,9,sumop);
+
+        st.displaySegTree();
+
+        System.out.println(Arrays.toString(ipArr));
+*/
+        // lazy propagation testing
+
+        SegTree st2 = new SegTree(ipArr,sumop,true);
+
+        st2.rangeUpdateLazy(ipArr,0,2,10,sumop);
+
+        st2.displaySegTree();
+        st2.displayLazyTree();
+
+        int ans = st2.rangeQueryLazy(ipArr,1,1,sumop);
+        System.out.println(ans);
+
+        st2.displaySegTree();
+        st2.displayLazyTree();
     }
 }
