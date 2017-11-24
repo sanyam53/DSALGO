08b6097c913ef6c8e45e8a044da7e87da4cdba04
tolani <tolani.mahesh72@gmail.com>
2017-11-24 18:04:04 +0530

    modified adj matrix repn a bit

--- src/com/tolani/Graphs/Graph1.java
+++ src/com/tolani/Graphs/Graph1.java
@@ -1,26 +1,32 @@
 package com.tolani.Graphs;
 
+//import com.tolani.Graphs.GraphType;
+
 public class Graph1 {
 
     int n;                        // #of vertices
-
     boolean[][] adjMatrix;            // 2d array
+    GraphType gtype;
 
-    Graph1(int size)
+    Graph1(int size,GraphType gt)
     {
         this.n = size;
-
         adjMatrix = new boolean[n][n];
 
+        gtype = gt;
     }
 
     public void addEdge(int u , int v)
     {
-        adjMatrix[u][v] = true;
+        if (gtype == GraphType.DIRECTED) {
+            adjMatrix[u][v] = true;
+        }
 
-       // if its undirected graph then
-
-        adjMatrix[v][u] = true;
+        else      // if its undirected graph then
+        {
+            adjMatrix[u][v] = true;
+            adjMatrix[v][u] = true;
+        }
     }
 
     public void displayGraph()
@@ -32,8 +38,6 @@ public class Graph1 {
 
             for(int i=0 ; i < adjMatrix.length ; i++)
             {
-
-
                 if(adjMatrix[i][j])System.out.print("1 ");
                 else System.out.print("0 ");
             }
