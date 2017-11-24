8a4aa51ccf300c2a093dfd262cbf1243f44b2124
tolani <tolani.mahesh72@gmail.com>
2017-11-25 00:00:36 +0530

    Problem : if thr exist a path bw pair (i,j)

---
+++ src/com/tolani/Graphs_WIth_DP/p1_WarshallsAlgo.java
@@ -0,0 +1,108 @@
+package com.tolani.Graphs_WIth_DP;
+
+import com.tolani.Graphs.Graph1;
+import com.tolani.Graphs.GraphType;
+import com.tolani.Graphs.IsGraphWeighted;
+
+import java.util.Arrays;
+
+public class p1_WarshallsAlgo {
+
+    public static void main(String[] args)
+    {
+        Graph1 g = new Graph1(4,GraphType.DIRECTED, IsGraphWeighted.NON_WEIGHTED);
+
+        g.addEdge(0,1);
+        g.addEdge(0,3);
+        g.addEdge(1,0);
+        g.addEdge(2,0);
+        g.addEdge(2,1);
+        g.addEdge(2,3);
+        g.addEdge(3,1);
+
+        //g.displayGraph();
+
+        warshallsAlgo(g);
+    }
+
+
+    public static void warshallsAlgo(Graph1 g)
+    {
+        int n = g.getN();    // no of vertices getter method
+
+        int[][] T = new int[n][n];
+
+        // for k = 0 : means no intermediate nodes allwd
+        for(int i=0 ; i < n ; i++)
+        {
+            for(int j =0 ; j < n ;j++)
+            {
+                T[i][j] = g.adjMatrix[i][j];
+            }
+        }
+
+        //displayBooleanMatrix(T);
+
+        for(int k = 1; k <= n ; k++)    // at most 'n' vertices are allwd as an intermediate vertices
+        {
+            // below is we are just forming a T^k(i,j) for the values of 'k' : which will overwrite the previous T^k-1(i,j) formed
+            for(int i=0 ; i < n ;i++)
+            {
+                for(int j=0 ; j < n ; j++)
+                {
+                    // now 'k' is implicit here
+
+                    if( (T[i][j] == 1) || (T[i][k-1] == 1 && T[k-1][j] == 1))   // whn 'k = 4' means first 4 vertices(0 to 3) are allwd
+                    {                                                                    // so we will chck (i to k) means it is (i to 3)
+                        T[i][j] = 1;                                                // as we indexed our vertices , same for (j to k)
+                    }
+                    else
+                    {
+                        T[i][j] = 0;
+                    }
+                }
+            }
+
+        }
+
+        displayMatrix(T);
+    }
+
+
+   /* public static void displayBooleanMatrix(boolean[][] adjMatrix) {
+
+        for(int i=0 ; i < adjMatrix.length ; i++)
+        {
+            System.out.print(i + " : ");
+
+            for(int j=0 ; j < adjMatrix.length ; j++)
+            {
+                if(adjMatrix[i][j]) System.out.print("1 ");
+                else System.out.print("0 ");
+            }
+
+            System.out.println();
+        }
+    }
+*/
+    public static void displayMatrix(int[][] matrix)
+    {
+        int k=0 ;
+
+        for(int i[] : matrix)
+        {
+            System.out.print(k + " : ");
+            k++;
+            for(int j : i)
+            {
+                if(j == 99999)
+                {
+                    System.out.print("INF ");
+                }
+                else System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+    }
+
+}
