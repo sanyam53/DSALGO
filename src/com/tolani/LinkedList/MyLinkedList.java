fbc1c48ff5f5485b1a545cad09c2155b26db2dca
tolani <tolani.mahesh72@gmail.com>
2018-05-17 12:36:37 +0530

    added reverse linked list iterative method

--- src/com/tolani/LinkedList/MyLinkedList.java
+++ src/com/tolani/LinkedList/MyLinkedList.java
@@ -1,11 +1,13 @@
 package com.tolani.LinkedList;
 
+// class that holds one instance of linkedlist , you could create more than one linked list objects
+
 public class MyLinkedList {
 
     public Node head;   // head of the linked list which points to the type Node
     public int size;     // size of the linked list means : #of nodes
 
-    public static class Node            // static nested class : so tht we cn drctly access it without creting outer class obj in main method
+    public static class Node       // static nested class : so tht we cn drctly access it without creating outer class obj in main method
     {
         public int data;     // data
         public Node next;    // link to the next node
@@ -96,4 +98,39 @@ public class MyLinkedList {
         }
     }
 
-}
+    // iterative code : to reverse a given linked list : look at simulation in screenshot
+    public boolean reverseList()
+    {
+        if(head == null) return false;
+
+        Node prevN,currN,nextN;
+
+        // intializaiton
+        prevN = null;
+        currN = this.head;     // as u call this method on list object , we access head ptr of tht list
+        nextN = this.head.next;
+
+        // traversing
+
+        while(currN != null)
+        {
+            currN.next = prevN;
+            prevN = currN;
+            currN = nextN;
+
+            if(nextN!= null)
+            {
+                nextN = nextN.next;
+            }
+        }
+
+        // change head of the linkedlist by the ptr pointing to the last node at the end : look at simulation for this
+        this.head = prevN;
+
+        return true;
+        }
+
+
+
+    }
+
